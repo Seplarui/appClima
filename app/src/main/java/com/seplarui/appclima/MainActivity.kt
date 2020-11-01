e4d87b22da9fb9cc1fcfd8f6cb6686c6485adb24
Sebas <seplarui@gmail.com>
2020-11-01 20:55:35 +0100

    ok

--- app/src/main/java/com/seplarui/appclima/MainActivity.kt
+++ app/src/main/java/com/seplarui/appclima/MainActivity.kt
@@ -2,10 +2,43 @@ package com.seplarui.appclima
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var tvCiudad:TextView? =null
+    var tvGrados:TextView?=null
+    var tvEstatus:TextView?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvCiudad = findViewById(R.id.tvCiudad)
+        tvGrados = findViewById(R.id.tvGrados)
+        tvEstatus = findViewById(R.id.tvEstatus)
+
+        val ciudad = intent.getStringExtra("com.seplarui.appclima.ciudades.CIUDAD")
+        Toast.makeText(this, ciudad, Toast.LENGTH_SHORT).show()
+
+        val ciudadmx = Ciudad("Ciudad de México", 15, "Soleado")
+        val ciudadBerlin = Ciudad("Ciudad de Berlin", 20, "Cielo despejado")
+
+        if(ciudad=="ciudad-mexico") {
+            //Mostrar informacion ciudadmx
+            tvCiudad?.text=ciudadmx.nombre
+            tvGrados?.text=ciudadmx.grados.toString() + "º"
+            tvEstatus?.text=ciudadmx.estatus
+
+        } else if(ciudad =="ciudad-berlin") {
+            tvCiudad?.text=ciudadBerlin.nombre
+            tvEstatus?.text=ciudadBerlin.estatus
+            tvGrados?.text=ciudadBerlin.grados.toString() + "º"
+
+        } else {
+            Toast.makeText(this, "No se encuentra la información", Toast.LENGTH_SHORT).show()
+        }
     }
 }
